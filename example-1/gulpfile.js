e6fdd50aa9c4f90823266b26d8e68a599447e6a5
Kevin Chisholm <kevin@kevinchisholm.com>
2015-05-02 14:06:19 -0400

    add comments to gulpfile.js

--- example-1/gulpfile.js
+++ example-1/gulpfile.js
@@ -1,8 +1,16 @@
+//get references to the two modules that we need
 var gulp = require('gulp'),
     concat = require('gulp-concat');
 
+//create a default gulp task
 gulp.task('default', function() {
+  
+  //return the file stream
   return gulp
+
+    //read the files into memory
     .src(['src/js/file-1.js','src/js/file-2.js','src/js/file-3.js'])
+
+    //write the files from memory to disk
     .pipe(gulp.dest('build/js')); 
 });
