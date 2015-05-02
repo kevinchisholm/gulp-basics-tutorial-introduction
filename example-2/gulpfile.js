fca3fa500fe6ce8da28a87e7cbef90410c58fce4
Kevin Chisholm <kevin@kevinchisholm.com>
2015-05-02 15:10:09 -0400

    remove duplicate code

--- example-2/gulpfile.js
+++ example-2/gulpfile.js
@@ -1,13 +1,3 @@
-var gulp = require('gulp'),
-    concat = require('gulp-concat');
-
-gulp.task('default', function() {
-  return gulp
-    .src(['src/js/file-1.js','src/js/file-2.js','src/js/file-3.js'])
-    
-    .pipe(gulp.dest('build/js')); 
-});
-
 //get references to the two modules that we need
 var gulp = require('gulp'),
     concat = require('gulp-concat');
