1004314e86f7aec2cf139406980d170a011f0749
Kevin Chisholm <kevin@kevinchisholm.com>
2015-05-02 14:06:47 -0400

    add example-2

---
+++ example-2/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat');
+
+gulp.task('default', function() {
+  return gulp
+    .src(['src/js/file-1.js','src/js/file-2.js','src/js/file-3.js'])
+    
+    .pipe(gulp.dest('build/js')); 
+});
+
+//get references to the two modules that we need
+var gulp = require('gulp'),
+    concat = require('gulp-concat');
+
+//create a default gulp task
+gulp.task('default', function() {
+  
+  //return the file stream
+  return gulp
+
+    //read the files into memory
+    .src(['src/js/file-1.js','src/js/file-2.js','src/js/file-3.js'])
+
+    //pipe the file to the contact module,
+    //which creates one concatenated file
+    .pipe(concat('scripts-all.js'))
+
+    //write the files from memory to disk
+    .pipe(gulp.dest('build/js')); 
+});
